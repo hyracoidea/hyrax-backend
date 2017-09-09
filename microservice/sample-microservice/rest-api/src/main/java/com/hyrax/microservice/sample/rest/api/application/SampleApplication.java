bc79a5e487fc8743f44a68b8c6559572124019f2
gabichelsea <gabilampard@gmail.com>
2017-09-09 13:08:20 +0200

    #3 - Create spring-boot sample-microservice based application skeleton

---
+++ microservice/sample-microservice/rest-api/src/main/java/com/hyrax/microservice/sample/rest/api/application/SampleApplication.java
@@ -0,0 +1,12 @@
+package com.hyrax.microservice.sample.rest.api.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(SampleApplication.class, args);
+    }
+}
