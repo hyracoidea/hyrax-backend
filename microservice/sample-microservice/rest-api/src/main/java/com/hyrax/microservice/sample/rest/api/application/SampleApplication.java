9fdae3f98491126d36a8cf8c9e35a56f8536d08e
gabichelsea <gabilampard@gmail.com>
2017-10-22 12:50:56 +0200

    #12- Introduce Swagger for REST-API documentation

--- microservice/sample-microservice/rest-api/src/main/java/com/hyrax/microservice/sample/rest/api/application/SampleApplication.java
+++ microservice/sample-microservice/rest-api/src/main/java/com/hyrax/microservice/sample/rest/api/application/SampleApplication.java
@@ -1,12 +1,13 @@
 package com.hyrax.microservice.sample.rest.api.application;
 
 import com.hyrax.microservice.sample.rest.api.configuration.RESTApiModuleConfiguration;
+import com.hyrax.microservice.sample.rest.api.configuration.SwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(RESTApiModuleConfiguration.class)
+@Import({RESTApiModuleConfiguration.class, SwaggerConfig.class})
 public class SampleApplication {
 
     public static void main(final String[] args) {
