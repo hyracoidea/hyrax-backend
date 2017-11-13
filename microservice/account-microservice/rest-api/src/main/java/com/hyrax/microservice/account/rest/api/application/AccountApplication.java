736ad7b644f9ee2f45922e08989d8bfa0c02d374
gabichelsea <gabilampard@gmail.com>
2017-11-13 20:57:41 +0100

    #18 - Create spring-boot account-microservice based application skeleton

---
+++ microservice/account-microservice/rest-api/src/main/java/com/hyrax/microservice/account/rest/api/application/AccountApplication.java
@@ -0,0 +1,12 @@
+package com.hyrax.microservice.account.rest.api.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(AccountApplication.class, args);
+    }
+}
