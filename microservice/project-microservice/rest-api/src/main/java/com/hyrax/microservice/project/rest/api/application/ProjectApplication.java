3e189b3d86ab768aac8bcbad8a17dea812a992ff
gabichelsea <gabilampard@gmail.com>
2018-02-17 09:13:56 +0100

    #37 - Create spring-boot project-microservice based application skeleton

---
+++ microservice/project-microservice/rest-api/src/main/java/com/hyrax/microservice/project/rest/api/application/ProjectApplication.java
@@ -0,0 +1,12 @@
+package com.hyrax.microservice.project.rest.api.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(ProjectApplication.class, args);
+    }
+}
