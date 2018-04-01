6a7e0f0b85d03b26fb02df8acb48866e6a1c611e
gabichelsea <gabilampard@gmail.com>
2018-04-01 18:39:08 +0200

    #68 - Fix travis build

--- spring-boot-starter/scripts/build_spring_boot_starters.sh
+++ spring-boot-starter/scripts/build_spring_boot_starters.sh
@@ -5,6 +5,13 @@ sh build_spring_boot_starter_authentication.sh
 
 if [ $? -ne 0 ]; then
     exit 1
+fi
+
+cd ../../swagger/scripts
+sh build_spring_boot_starter_swagger.sh
+
+if [ $? -ne 0 ]; then
+    exit 1
 else
     exit 0
 fi
