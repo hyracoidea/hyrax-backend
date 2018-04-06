bed1fb3a000c4cf188fc2cc0bc59fa796639489f
gabichelsea <gabilampard@gmail.com>
2018-04-06 21:06:48 +0200

    #104 - Integrate email action settings REST clients into the account-microservice

--- client/email-client/client-api-impl/src/main/java/com/hyrax/client/email/api/service/impl/EmailEventSubscriptionRESTClient.java
+++ client/email-client/client-api-impl/src/main/java/com/hyrax/client/email/api/service/impl/EmailEventSubscriptionRESTClient.java
@@ -3,8 +3,6 @@ package com.hyrax.client.email.api.service.impl;
 import com.hyrax.client.email.api.properties.EmailEventSubscriptionRESTClientProperties;
 import com.hyrax.client.email.api.request.BaseEventSubscriptionRequest;
 import lombok.AllArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.Entity;
@@ -14,14 +12,11 @@ import javax.ws.rs.core.Response;
 @AllArgsConstructor
 public class EmailEventSubscriptionRESTClient<T extends BaseEventSubscriptionRequest> {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(EmailEventSubscriptionRESTClient.class);
-
     private final Client client;
 
     private final EmailEventSubscriptionRESTClientProperties emailEventSubscriptionRESTClientProperties;
 
     public Response callEmailEventSubscriptionRESTEndpoint(final String path, final T request) {
-        LOGGER.info("Target: {}", emailEventSubscriptionRESTClientProperties.getServiceUrl() + path);
         return client.target(emailEventSubscriptionRESTClientProperties.getServiceUrl())
                 .path(path)
                 .request(MediaType.APPLICATION_JSON_TYPE)
