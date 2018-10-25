bc4a35e26bf3aa3a79fcee80b1659c3febfc0495
Gabor Ivanyi-Nagy <gabilampard@gmail.com>
2018-10-25 20:24:06 +0200

    Extends WebMvcConfigurerAdapter instead of implements WebMvcConfigurer

--- spring-boot-starter/authentication/authentication-service/src/main/java/com/hyrax/spring/boot/starter/authentication/configuration/CorsConfiguration.java
+++ spring-boot-starter/authentication/authentication-service/src/main/java/com/hyrax/spring/boot/starter/authentication/configuration/CorsConfiguration.java
@@ -2,10 +2,10 @@ package com.hyrax.spring.boot.starter.authentication.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class CorsConfiguration implements WebMvcConfigurer {
+public class CorsConfiguration extends WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
